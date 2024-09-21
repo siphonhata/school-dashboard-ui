6b72d038b6808d470b4c68722bdb5d2b73703a75
siphonhata <siphonhata@gmail.com>
2024-09-21 09:43:07 +0200

    chart components

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,4 +1,9 @@
-import UserCard from "../../../components/UserCard";
+import UserCard from "@/components/UserCard";
+import CountChart from "@/components/CountChart";
+import AttendanceChart from "@/components/AttendanceChart ";
+import FinanceChart from "@/components/FinanceChart";
+import EventCalendar from "@/components/EventCalendar";
+import Announcements from "@/components/Announcements";
 
 const AdminPage = () => {
   return (
@@ -12,8 +17,27 @@ const AdminPage = () => {
           <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
+        </div>
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
       </div>
-      <div className="w-full lg:w-1/3">r</div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements/>
+      </div>
     </div>
   );
 };
